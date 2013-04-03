7329d34836784a534259bdc5fb3d96c9a605f039
Brian Lonsdorf <brian@looprecur.com>
2013-04-03 09:33:35 -0700

    whoops

---
+++ typeclasses/support/types.js
@@ -0,0 +1,31 @@
+Constructor = function(f) {
+  var x = function(){
+    if(!(this instanceof x)){
+      var inst = new x();
+      f.apply(inst, arguments);
+      return inst;
+    }
+    f.apply(this, arguments);
+  };
+
+  return x;
+}
+
+makeType = function(f) {
+  f = f || function(v){ this.val = v; }
+  return Constructor(f);
+}
+
+// Some default types
+
+Maybe = makeType();
+
+// _Either is a temporary hack until we can keep a consistent prototype during autoCurry
+_Either = Constructor(function(left, right){
+  this.left = left;
+  this.right = right;                    
+});
+
+Either = function(left, right){
+  return _Either(left, right);
+}.autoCurry();
